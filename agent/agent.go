677a7cf855e52997525c085c9c9c1b54f3f58780
Nikita Chebykin <nikita@chebyk.in>
2018-04-12 13:34:12 +0200

    Add agent logging

--- agent/agent.go
+++ agent/agent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -11,6 +12,7 @@ import (
 	"math/big"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
 	"sync"
 	"time"
@@ -22,6 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	ethrpc "github.com/ethereum/go-ethereum/rpc"
 	"github.com/gorilla/mux"
+	"github.com/pubnub/go/messaging"
 	web3 "github.com/regcostajr/go-web3"
 	"github.com/regcostajr/go-web3/dto"
 	"github.com/regcostajr/go-web3/providers"
@@ -30,6 +33,13 @@ import (
 var config *Configuration
 var chainMap *ChainMap
 var statsdClient statsd.Statter
+var pubnub *messaging.Pubnub
+
+const (
+	pnPubKey  = "demo"
+	pnSubKey  = "demo"
+	pnChannel = "statistics"
+)
 
 // Agent starts http server to receive instructions from the master.
 // It sends statistics to statsd daemon.
@@ -85,6 +95,8 @@ func main() {
 		log.Panicln("Failed to connect statsd server:", err)
 	}
 
+	pubnub = messaging.NewPubnub(pnPubKey, pnSubKey, "", "", false, "", nil)
+
 	defer statsdClient.Close()
 
 	server()
@@ -94,7 +106,8 @@ func server() {
 	r := mux.NewRouter()
 	r.Path("/ethSendRaw").HandlerFunc(ethSendRaw)
 	r.Path("/personalSignAndSend").HandlerFunc(personalSignAndSendHandler)
-	r.Path("/sendInternalSolidity").HandlerFunc(sendInternalSolidityHandler)
+	r.Path("/progress").HandlerFunc(progressHandler)
+	r.Path("/workpackage").HandlerFunc(workpackageHandler)
 	addr := "0.0.0.0:8080"
 
 	srv := &http.Server{
@@ -114,8 +127,27 @@ func personalSignAndSendHandler(w http.ResponseWriter, r *http.Request) {
 	personalHandler(personalSignAndSendWorker, w, r)
 }
 
-func sendInternalSolidityHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+func progressHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func workpackageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Receive workload")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	success := make(chan []byte)
+	error := make(chan []byte)
+
+	go pubnub.Publish("work", struct {
+		Name      string `json:"name"`
+		Payload   string `json:"payload"`
+		Timestamp int64  `json:"timestamp"`
+	}{
+		Name:      config.Me.Name,
+		Payload:   string(body),
+		Timestamp: time.Now().Unix() * 1000,
+	}, success, error)
 }
 
 func personalHandler(worker Worker, w http.ResponseWriter, r *http.Request) {
@@ -332,10 +364,9 @@ func ethSendRaw(w http.ResponseWriter, r *http.Request) {
 	signer := types.NewEIP155Signer(big.NewInt(int64(15054)))
 
 	for i := 0; i < count; i++ {
-		tx := types.NewTransaction(nonce, common.StringToAddress(chainMap.Peers[0]),
+		tx := types.NewTransaction(nonce, common.BytesToAddress([]byte(chainMap.Peers[0])),
 			big.NewInt(1E16),
 			uint64(21000), big.NewInt(1E9), []byte(""))
-
 
 		signed_tx, _ := types.SignTx(tx, signer, key.PrivateKey)
 		txs[i] = signed_tx
@@ -362,9 +393,12 @@ func ethSendRaw(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 
+	msgs := make(chan counterMessage, 100*1000)
+	go counter(r.Context(), msgs)
+
 	log.Println("Launching", config.WorkersCount, "workers...")
 	for w := 0; w < config.WorkersCount; w++ {
-		go ethSendRawWorker(jobsCh, resultsCh)
+		go ethSendRawWorker(uint8(w), msgs, jobsCh, resultsCh)
 	}
 
 	log.Println("Scheduling jobs...")
@@ -402,7 +436,9 @@ func ethSendRaw(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(resultBytes))
 }
 
-func ethSendRawWorker(jobs <-chan *types.Transaction, results chan<- sendResult) {
+func ethSendRawWorker(id uint8, countMsgs chan<- counterMessage,
+	jobs <-chan *types.Transaction, results chan<- sendResult) {
+
 	client, err := getClient(fmt.Sprintf("http://%s", config.Endpoints.RPC))
 	if err != nil {
 		log.Panic(err)
@@ -410,9 +446,11 @@ func ethSendRawWorker(jobs <-chan *types.Transaction, results chan<- sendResult)
 
 	for tx := range jobs {
 		err := client.SendTransaction(context.Background(), tx)
+		countMsgs <- counterMessage{id}
 		if err != nil {
 			log.Println(err)
 			results <- sendResult{true, err.Error()}
+			return
 		}
 		results <- sendResult{false, "ok"}
 	}
@@ -458,6 +496,85 @@ func getClient(rawUrl string) (*ethclient.Client, error) {
 	return ethclient.NewClient(c), nil
 }
 
+func counter(ctx context.Context, msgs <-chan counterMessage) {
+	store := make(map[uint8]int)
+	total := 0
+	pubnub := messaging.NewPubnub(pnPubKey, pnSubKey, "", "", false, "", nil)
+
+	writeTick := func() {
+		var buffer bytes.Buffer
+		now := time.Now()
+		buffer.WriteString(now.Format("15:04:05 "))
+
+		keys := make([]int, len(store))
+		i := 0
+
+		for k := range store {
+			keys[i] = int(k)
+			i++
+		}
+		sort.Ints(keys)
+
+		for k := range keys {
+			v := store[uint8(k)]
+			buffer.WriteString(fmt.Sprintf("W%d: %d; ", k, v))
+		}
+
+		buffer.WriteString(fmt.Sprintf("total: %d\n", total))
+
+		f, err := os.OpenFile(config.TickerLog,
+			os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+		defer f.Close()
+
+		if err != nil {
+			log.Panic(err)
+		}
+
+		success := make(chan []byte)
+		error := make(chan []byte)
+
+		go pubnub.Publish(pnChannel, struct {
+			Eon       map[string]int `json:"eon"`
+			Timestamp int64          `json:"_eonDatetime"`
+		}{
+			Eon: map[string]int{
+				config.Me.Name: total,
+			},
+			Timestamp: time.Now().Unix() * 1000,
+		}, success, error)
+
+		_, err = f.Write(buffer.Bytes())
+		if err != nil {
+			log.Panic(err)
+		}
+
+		store = make(map[uint8]int)
+		total = 0
+	}
+
+	ticker := time.NewTicker(time.Millisecond * 1000)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				ticker.Stop()
+				writeTick()
+				return
+			case <-ticker.C:
+				writeTick()
+			case msg := <-msgs:
+				store[msg.WorkerID]++
+				total++
+			}
+		}
+	}()
+}
+
+type counterMessage struct {
+	WorkerID uint8
+}
+
 type sendOpts struct {
 	Val       *big.Int
 	Addressee string
@@ -491,6 +608,7 @@ type Configuration struct {
 	}
 	PeerKey      string
 	Logs         string
+	TickerLog    string
 	Secret       string
 	TxLimit      int
 	WorkersCount int
