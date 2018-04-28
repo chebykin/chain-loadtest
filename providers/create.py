348072ee43794911b1fdda5d63a1b4ebb1f62653
Nikita Chebykin <nikita@chebyk.in>
2018-04-28 07:33:54 +0200

    Add option to customize size and location

--- providers/create.py
+++ providers/create.py
@@ -57,18 +57,49 @@ class CreateNode(threading.Thread):
             raise Exception('Provider not specified for', self.name)
 
         res = driver.create_node(self.config.name,
-                                      self.config.size,
-                                      self.config.image,
-                                      location=self.config.location,
-                                      ex_tags=self.config.tags)
+                                 self.config.size,
+                                 self.config.image,
+                                 location=self.config.location,
+                                 ex_tags=self.config.tags)
         print(res)
 
 
 instances = []
-size = chainMap['defaults']['size']
-location = chainMap['defaults']['location']
+
 image = chainMap['defaults']['image']
 provider = chainMap['defaults']['provider']
+
+defaults = chainMap['defaults']
+
+peer_size = defaults['size']
+peer_location = defaults['location']
+
+validator_size = defaults['size']
+validator_location = defaults['location']
+
+observer_size = defaults['size']
+observer_location = defaults['location']
+
+if 'peers' in chainMap['defaults']:
+    peer_defaults = chainMap['defaults']['peers']
+    if 'size' in peer_defaults:
+        peer_size = peer_defaults['size']
+    if 'location' in peer_defaults:
+        peer_location = peer_defaults['location']
+
+if 'validators' in chainMap['defaults']:
+    validator_defaults = chainMap['defaults']['validators']
+    if 'size' in validator_defaults:
+        validator_size = validator_defaults['size']
+    if 'location' in validator_defaults:
+        validator_location = validator_defaults['location']
+
+if 'observers' in chainMap['defaults']:
+    observer_defaults = chainMap['defaults']['observers']
+    if 'size' in observer_defaults:
+        observer_size = observer_defaults['size']
+    if 'location' in observer_defaults:
+        observer_location = observer_defaults['location']
 
 peer_tags = ['peer-node', 'tn-{:d}'.format(allConfig['timestamp'])]
 validator_tags = ['validator-node', 'tn-{:d}'.format(allConfig['timestamp'])]
@@ -79,10 +110,28 @@ print(chainMap)
 for key, val in chainMap.items():
     if key.startswith('peer-'):
         tags = peer_tags
+
+        if val is None:
+            val = {}
+
+        size = val['size'] if 'size' in val else peer_size
+        location = val['location'] if 'location' in val else peer_location
     elif key.startswith('validator-'):
         tags = validator_tags
+
+        if val is None:
+            val = {}
+
+        size = val['size'] if 'size' in val else validator_size
+        location = val['location'] if 'location' in val else validator_location
     elif key.startswith('observer-'):
         tags = observer_tags
+
+        if val is None:
+            val = {}
+
+        size = val['size'] if 'size' in val else observer_size
+        location = val['location'] if 'location' in val else observer_location
     else:
         continue
 
